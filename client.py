fe0ac1ff1ec9c9625274f7c85608316fdc1108a4
jerre231 <joaoscofield.20231@poli.ufrj.br>
2023-10-28 21:51:20 -0300

    GUI

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
-import socket 
+import PySimpleGUI as sg
+import socket
 
 HEADER = 64
 PORT = 18000
-FORMAT ='utf-8'
+FORMAT = 'utf-8'
 SERVER = 'localhost'
 ADDR = (SERVER, PORT)
 DISCONNECT = ':D'
@@ -10,30 +11,44 @@ DISCONNECT = ':D'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg):
+def send_message(msg):
+    message = msg.encode(FORMAT)
+    msg_len = len(message)
+    send_len = str(msg_len).encode(FORMAT)
+    send_len += b' ' * (HEADER - len(send_len))
+    client.send(send_len)
+    client.send(message)
+    return client.recv(2048).decode(FORMAT)
 
-        message = msg.encode(FORMAT)
-        msg_len = len(message)
-        send_len = str(msg_len).encode(FORMAT)
-        send_len += b' ' * (HEADER - len(send_len))
-        client.send(send_len)
-        client.send(message)
-        print(client.recv(2048).decode(FORMAT))
-        
-while True:
-    entry = input('Sua mensagem: ')
-    if entry == DISCONNECT:
-        send(entry)
-        print('Desconctando...')
-        break
-    
-    else:
-        send(entry)
-    
-# if __name__=='__main__':
-#     quantidade = int(input('Quantas msgs vc vai mandar? '))
-#     for i in range(quantidade):
-#         entry = input('Sua mensagem: ')
-#         send(entry)
+def main():
+    sg.theme('DarkBlue2')
 
+    layout = [
+        [sg.Text('Digite uma mensagem:')],
+        [sg.InputText(key='input_text')],
+        [sg.Button('Send')],
+    ]
 
+    window = sg.Window('Chat Cliente', layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED:
+            send_message(DISCONNECT)
+            break
+        elif event == 'Send':
+            message = values['input_text']
+            if message == DISCONNECT:
+                send_message(message)
+                sg.popup('Desconectando...')
+                break
+            else:
+                response = send_message(message)
+                sg.popup(f'Resposta do servidor: {response}')
+
+    window.close()
+    client.close()
+
+if __name__ == '__main__':
+    main()
